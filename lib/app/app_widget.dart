a626013547b1ee053721285eff7dc6bb9deec133
Eloi Matos <eloi_matos@hotmail.com>
2021-09-14 18:59:32 -0300

    apply pt-BR locale

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('pt', 'BR')
+      ],
       title: 'Salutis Audit',
       theme: ThemeData(primarySwatch: Colors.pink),
     ).modular();
