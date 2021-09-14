be7bc06438f3873c9227b1d5418ae5851f9a6ff2
Eloi Matos <eloi_matos@hotmail.com>
2021-09-14 18:54:05 -0300

    refactor formulario entity

--- lib/app/modules/home/domain/entities/formulario.dart
+++ lib/app/modules/home/domain/entities/formulario.dart
@@ -1,10 +1,16 @@
+import 'package:mobx/mobx.dart';
+
+import 'agrupamento.dart';
+
 class Formulario {
-  final int id;
-  final int idade;
+  final String id;
+  final String idade;
   final String chQuestionario;
   final DateTime prazoResposta;
   final String versao;
-  //final List<Agrupamento> agrupamentos;
+  
+  @observable
+  final List<Agrupamento> agrupamentos;
   final String sexo;
   final String beneficiario;
   final String chAgendamento;
@@ -20,6 +26,7 @@ class Formulario {
     required this.chQuestionario,
     required this.prazoResposta,
     required this.versao,
+    required this.agrupamentos,
     required this.sexo,
     required this.beneficiario,
     required this.chAgendamento,
@@ -30,21 +37,70 @@ class Formulario {
     required this.operadora,
   });
 
-  factory Formulario.fromJson(Map<String, dynamic> json) {
-    return Formulario(
-      id: int.parse(json['id']),
-      idade: int.parse(json['idade']),
-      chQuestionario: json['chQuestionario'],
-      prazoResposta: DateTime.parse(json['prazoResposta']),
-      versao: json['versao'],
-      sexo: json['sexo'],
-      beneficiario: json['beneficiario'],
-      chAgendamento: json['chAgendamento'],
-      nome: json['nome'],
-      hospital: json['hospital'],
-      baseOrigem: json['baseOrigem'],
-      agendamento: DateTime.parse(json['agendamento']),
-      operadora: json['operadora']
+  Formulario copyWith({
+    String? id,
+    String? idade,
+    String? chQuestionario,
+    DateTime? prazoResposta,
+    String? versao,
+    List<Agrupamento>? agrupamentos,
+    String? sexo,
+    String? beneficiario,
+    String? chAgendamento,
+    String? nome,
+    String? hospital,
+    String? baseOrigem,
+    DateTime? agendamento,
+    String? operadora,
+  }) => 
+    Formulario(
+      id: id ?? this.id,
+      idade: idade ?? this.idade,
+      chQuestionario: chQuestionario ?? this.chQuestionario,
+      prazoResposta: prazoResposta ?? this.prazoResposta,
+      versao: versao ?? this.versao,
+      agrupamentos: agrupamentos ?? this.agrupamentos,
+      sexo: sexo ?? this.sexo,
+      beneficiario: beneficiario ?? this.beneficiario,
+      chAgendamento: chAgendamento ?? this.chAgendamento,
+      nome: nome ?? this.nome,
+      hospital: hospital ?? this.hospital,
+      baseOrigem: baseOrigem ?? this.baseOrigem,
+      agendamento: agendamento ?? this.agendamento,
+      operadora: operadora ?? this.operadora,
     );
-  }
+
+  factory Formulario.fromJson(Map<String, dynamic> json) => Formulario(
+    id: json["id"],
+    idade: json["idade"],
+    chQuestionario: json["chQuestionario"],
+    prazoResposta: DateTime.parse(json["prazoResposta"]),
+    versao: json["versao"],
+    agrupamentos: List<Agrupamento>.from(json["agrupamentos"].map((x) => Agrupamento.fromJson(x))),
+    sexo: json["sexo"],
+    beneficiario: json["beneficiario"],
+    chAgendamento: json["chAgendamento"],
+    nome: json["nome"],
+    hospital: json["hospital"],
+    baseOrigem: json["baseOrigem"],
+    agendamento: DateTime.parse(json["agendamento"]),
+    operadora: json["operadora"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "idade": idade,
+    "chQuestionario": chQuestionario,
+    "prazoResposta": prazoResposta.toIso8601String(),
+    "versao": versao,
+    "agrupamentos": List<dynamic>.from(agrupamentos.map((x) => x.toJson())),
+    "sexo": sexo,
+    "beneficiario": beneficiario,
+    "chAgendamento": chAgendamento,
+    "nome": nome,
+    "hospital": hospital,
+    "baseOrigem": baseOrigem,
+    "agendamento": agendamento.toIso8601String(),
+    "operadora": operadora,
+  };
 }
